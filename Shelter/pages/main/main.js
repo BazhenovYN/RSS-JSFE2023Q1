f91d68c7ee7b1b6e2eeb1311edec80d579df1ed2
BazhenovYN <BazhenovYN@gmail.com>
2023-03-25 20:46:15 +0300

    feat: finalization of all requirements

---
+++ Shelter/pages/main/main.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// --- Log ---
+
+const textLog = 
+  '1. При нажатии на кнопки: Gardens, Lawn, Planting происходит смена фокуса на услугах в разделе service +50' + '\n' + 
+  '2. В разделе prices реализовано 3 Accordion с информацией об услугах и ценах + 50' + '\n' +
+  '3. В разделе contacts реализован select с выбором городов +25';
+
+console.log(textLog);
