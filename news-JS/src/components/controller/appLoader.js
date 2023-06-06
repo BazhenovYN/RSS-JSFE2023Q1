30e89f81988e939f451208aaa2878f23690cefdd
BazhenovYN <BazhenovYN@gmail.com>
2023-06-06 23:12:48 +0200

    refactor: update dependencies

--- news-JS/src/components/controller/appLoader.js
+++ news-JS/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '0e8f1ad263c5464b98e871f58b7b783e', // получите свой ключ https://newsapi.org/
         });
     }
 }
