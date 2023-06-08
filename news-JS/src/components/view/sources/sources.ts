f5c9d3c560f117bbf0a66e2434a48d96a77c538b
BazhenovYN <BazhenovYN@gmail.com>
2023-06-08 17:05:09 +0200

    feat: migrate sources to TS

---
+++ news-JS/src/components/view/sources/sources.ts
@@ -0,0 +1,33 @@
+import { ISource } from '../../../types';
+import { getElement } from '../../../utils';
+import './sources.css';
+
+class Sources {
+  private sources: ISource[] = [];
+
+  public draw(data: ISource[] = []): void {
+    if (data.length === 0 && this.sources.length === 0) {
+      return;
+    }
+
+    this.sources = data;
+
+    const fragment: DocumentFragment = document.createDocumentFragment();
+    const sourceItemTemp = getElement<HTMLTemplateElement>(document, '#sourceItemTemp');
+
+    if (!sourceItemTemp) return;
+    
+    this.sources.forEach((item: ISource) => {
+      const sourceClone = sourceItemTemp.content.cloneNode(true) as DocumentFragment;
+
+      getElement<HTMLDivElement>(sourceClone, '.source__item-name').textContent = item.name;
+      getElement<HTMLDivElement>(sourceClone, '.source__item').setAttribute('data-source-id', item.id);
+
+      fragment.append(sourceClone);
+    });
+
+    getElement<HTMLDivElement>(document, '.sources').append(fragment);
+  }
+}
+
+export default Sources;
