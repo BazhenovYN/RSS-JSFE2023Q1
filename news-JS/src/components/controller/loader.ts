5ada068c49b31609c30a0993a30ad3640e3d7b95
BazhenovYN <BazhenovYN@gmail.com>
2023-06-08 17:22:15 +0200

    feat: migrate loader to TS

---
+++ news-JS/src/components/controller/loader.ts
@@ -0,0 +1,45 @@
+import { HttpMethod, Endpoint, Options, IDrawFunc } from '../../types';
+
+const noCallback = (): void => {
+  throw Error('No callback for GET response');
+};
+
+class Loader {
+  constructor(private baseLink: string, private options: Options) {
+    this.baseLink = baseLink;
+    this.options = options;
+  }
+
+  public getResp(endpoint: Endpoint, callback: IDrawFunc = noCallback, options: Options = {}): void {
+    this.load(HttpMethod.GET, endpoint, options, callback);
+  }
+
+  private makeUrl(options: Options, endpoint: Endpoint): string {
+    const urlOptions = { ...this.options, ...options };
+    let url = `${this.baseLink}${endpoint}?`;
+
+    Object.keys(urlOptions).forEach((key) => {
+      url += `${key}=${urlOptions[key]}&`;
+    });
+
+    return url.slice(0, -1);
+  }
+
+  private load(method: HttpMethod, endpoint: Endpoint, options: Options, callback: IDrawFunc): void {
+    fetch(this.makeUrl(options, endpoint), { method })
+      .then((response) => {
+        if (!response.ok) {
+          if (response.status === 401 || response.status === 404) {
+            throw Error(`${response.status} error: ${response.statusText}`);
+          }
+        }
+        return response.json();
+      })
+      .then((data) => callback(data))
+      .catch((error: Error) => {
+        throw error;
+      });
+  }
+}
+
+export default Loader;
