b1e71325ad628226585ef22fbdfed42621211321
BazhenovYN <BazhenovYN@gmail.com>
2023-06-12 16:12:31 +0200

    feat: update base link to newApi

--- news-JS/src/components/controller/appLoader.ts
+++ news-JS/src/components/controller/appLoader.ts
@@ -1,6 +1,6 @@
 import Loader from './loader';
 
-const BASE_LINK = 'https://newsapi.org/v2/';
+const BASE_LINK = 'https://rss-news-api.onrender.com/';
 const API_KEY = '0e8f1ad263c5464b98e871f58b7b783e';
 
 class AppLoader extends Loader {
