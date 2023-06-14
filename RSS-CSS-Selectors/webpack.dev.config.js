8d6159a2e5c534fc829bc97c13ccb1d2315622d8
BazhenovYN <BazhenovYN@gmail.com>
2023-06-14 11:01:11 +0200

    init: start rss-css-selectors task

---
+++ RSS-CSS-Selectors/webpack.dev.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  devServer: {
+    open: true,
+    port: 'auto',
+    static: {
+      directory: path.join(__dirname, 'src'),
+      watch: true,
+    },
+  },
+};
