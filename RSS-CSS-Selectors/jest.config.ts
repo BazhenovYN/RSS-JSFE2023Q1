169ddd2facb20577ce4b2ba3718874d7add80da5
BazhenovYN <BazhenovYN@gmail.com>
2023-07-01 20:28:34 +0200

    feat: installation and configuration of the Jest

---
+++ RSS-CSS-Selectors/jest.config.ts
@@ -0,0 +1,9 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleDirectories: ['node_modules', 'src'],
+  verbose: true,
+};
