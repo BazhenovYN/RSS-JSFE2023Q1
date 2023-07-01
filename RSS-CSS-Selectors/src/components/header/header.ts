6ae31cb3841c6f10a88dc306d1b3b1daaf3459bd
BazhenovYN <BazhenovYN@gmail.com>
2023-07-01 15:41:44 +0200

    feat: implement header

--- RSS-CSS-Selectors/src/components/header/header.ts
+++ RSS-CSS-Selectors/src/components/header/header.ts
@@ -2,6 +2,7 @@ import './_header.scss';
 import ElementCreator from "utils/element-creator";
 import { ElementParams } from "types";
 import View from "components/common/view";
+import logo from 'assets/images/chili.png';
 
 export default class HeaderView extends View {
   protected viewElement!: ElementCreator;
@@ -12,5 +13,32 @@ export default class HeaderView extends View {
       classes: ['header'],
     }
     super(params);
+    this.configureView();
+  }
+
+  private configureView(): void {
+    this.createGameLogo();
+  }
+
+  private createGameLogo(): void {
+    const gameLogo = new ElementCreator({tag: 'div', classes: ['game-title']});
+    
+    const img = new ElementCreator({
+      tag: 'img',
+      classes: ['game-logo'],
+      attributes: {
+        src: logo,
+        alt: 'RSSchool',
+      },
+    })
+
+    const title = new ElementCreator({
+      tag: 'span',
+      textContent: 'CSS Vegan',
+    });
+
+    gameLogo.addInnerElement(img);
+    gameLogo.addInnerElement(title);
+    this.viewElement.addInnerElement(gameLogo);
   }
 }
