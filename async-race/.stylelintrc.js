9431d8151462a4ae99f1f8ba84ac784a6696bf7e
BazhenovYN <BazhenovYN@gmail.com>
2023-07-13 14:25:08 +0200

    feat: add stylelint rules

--- async-race/.stylelintrc.js
+++ async-race/.stylelintrc.js
@@ -1,9 +1,9 @@
 module.exports = {
-  extends: [
-    'stylelint-config-recommended-scss',
-    'stylelint-config-recess-order'
-  ],
+  extends: ['stylelint-config-standard', 'stylelint-config-recommended-scss', 'stylelint-config-recess-order'],
   rules: {
-    'color-hex-length': 'long'
-  }
+    'color-hex-case': 'lower',
+    'color-hex-length': 'long',
+    'import-notation': 'string',
+    'property-no-vendor-prefix': null,
+  },
 };
