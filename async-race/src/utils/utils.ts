09f6a34b4c144d00f6dbdaa6bce91aaa0d882942
BazhenovYN <BazhenovYN@gmail.com>
2023-08-04 18:50:55 +0200

    refactor: create constants instead of numbers

--- async-race/src/utils/utils.ts
+++ async-race/src/utils/utils.ts
@@ -1,6 +1,8 @@
 import { emptyString } from 'app/consts';
 import type { Color } from 'types';
 
+const COLOR_LENGTH = 6; // #ffffff
+
 const brend = [
   'Ford',
   'Audi',
@@ -48,9 +50,10 @@ export function getRandomCarName(): string {
 
 export function getRandomColor(): Color {
   const letters = '23456789abcdef'; // hexadecimal numbers for limiting the spectrum to light colors
+  const {length} = letters;
   const color = [];
-  for (let i = 0; i < 6; i += 1) {
-    color.push(letters[Math.floor(Math.random() * letters.length)]);
+  for (let i = 0; i < COLOR_LENGTH; i += 1) {
+    color.push(letters[Math.floor(Math.random() * length)]);
   }
   return `#${color.join(emptyString)}`;
 }
